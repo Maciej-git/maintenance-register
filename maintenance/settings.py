26cd7e0235f13d4334c8e8c635068d48704ddfdd
Maciej Palczynski <maciej.palczynski@gmail.com>
2023-04-24 11:01:19 +0200

    apply app setup changes

--- maintenance/settings.py
+++ maintenance/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY = ''
+# SECRET_KEY = SECRET_KEY = 'django-insecure-gy6kg8v352prs0@#9axy_7@w*=h#qr)xq&26yek^egvw1=5)sn'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-gy6kg8v352prs0@#9axy_7@w*=h#qr)xq&26yek^egvw1=5)sn')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != False
 
 ALLOWED_HOSTS = []
 
